f59b0f63e472e211ada059ccfee3b74629ea900c
Egor Pristavka <pristavkaegor03@gmail.com>
2023-03-26 16:07:06 +0800

    Delete py-textures-processor directory

--- py-textures-processor/colortrans.py
+++
@@ -1,7 +0,0 @@
-import colortrans
-import numpy as np
-from PIL import Image
-content = np.array(Image.open("image_pseudonormal.png").convert('RGB'))
-reference = np.array(Image.open("image.jpg").convert('RGB'))
-result = Image.fromarray(colortrans.transfer_reinhard(content, reference))
-result.save("result.png")
